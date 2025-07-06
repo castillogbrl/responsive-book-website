487be74c29e507ae85f607e88d2a4257cc241e68
Gabriel Castillo <castillogabriel.cc@gmail.com>
2025-07-06 19:38:29 -0400

    adds login screen

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,40 @@
-/*=============== SEARCH ===============*/
+/* SEARCH */
 const searchContent = document.getElementById('search-content'),
       searchButton = document.getElementById('search-button'),
       searchClose = document.getElementById('search-close');
 
-/* Show menu */
+/* Show search */
 if (searchButton) {
     searchButton.addEventListener('click', () => {
         searchContent.classList.add('show-search')
     })
 }
 
-/* Hide menu*/
+/* Hide search */
 if (searchClose) {
     searchClose.addEventListener('click', () => {
         searchContent.classList.remove('show-search')
     })
 }
 
-/*=============== LOGIN ===============*/
+/* LOGIN */
+const loginContent = document.getElementById('login-content'), // Entire login content div
+      loginButton = document.getElementById('login-button'), // At nav, under actions
+      loginClose = document.getElementById('login-close'); // Inside content div, closes the screen
 
+/* Show login */
+if (loginButton) {
+    loginButton.addEventListener('click', () => {
+        loginContent.classList.add('show-login')
+    })
+}
+
+/* Hide login */
+if (loginClose) {
+    loginClose.addEventListener('click', () => {
+        loginContent.classList.remove('show-login')
+    })
+}
 
 /*=============== ADD SHADOW HEADER ===============*/
 
